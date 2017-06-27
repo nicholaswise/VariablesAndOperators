a3407690fed86d3487be1160faed6b5b5892a146
Nick Wise <nickcwise@gmail.com>
2017-06-27 10:55:27 -0400

    setup repo

---
+++ VariablesAndOperators/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VariablesAndOperators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
